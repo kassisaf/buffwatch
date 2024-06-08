cb288e19cd495284fab9608b980bf52bda788c7e
Andrew Kassis <kassisaf@gmail.com>
2024-06-07 21:43:25 -0700

    Add commands for adding/removing buffs

--- BuffWatch.lua
+++ BuffWatch.lua
@@ -1,24 +1,23 @@
 --[[
-BuffWatch
-Copyright © 2024 Zuri
+Copyright © 2024, Zuri
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-* Neither the name of craft nor the names of its contributors may be
-used to endorse or promote products derived from this software without
-specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of BuffWatch nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Mojo BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -34,11 +33,13 @@ _addon.commands = {
   'bw'
 }
 
+require('logger')
 config = require('config')
 
 windower.register_event('load', function()
   defaults = {
-    language = windower.ffxi.get_info().language
+    language = windower.ffxi.get_info().language,
+    profiles = {}
   }
   settings = config.load(defaults)
   lang = string.lower(settings.language)
@@ -58,11 +59,44 @@ windower.register_event('addon command', function(...)
     -- windower.add_to_chat(207, 'BuffWatch: //bw list')
     -- windower.add_to_chat(207, 'BuffWatch: //bw add <buff>')
     -- windower.add_to_chat(207, 'BuffWatch: //bw remove <buff>')
+  elseif cmd[1] == 'add' then
+    if cmd[2] == nil or cmd[3] == nil then
+      error('Invalid command. Try `//bw add <profile> <buff>`')
+    else
+      add_buff(cmd[2], cmd[3])
+    end
+  elseif cmd[1] == 'remove' then
+    if cmd[2] == nil or cmd[3] == nil then
+      error('Invalid command. Try `//bw remove <profile> <buff>`')
+    else
+      remove_buff(cmd[2], cmd[3])
+    end
   end
 end)
 
-function create_profile(profile_name)
+function add_buff(profile_name, buff_name)
+  -- Create profile if it doesn't already exist
+  if settings.profiles[profile_name] == nil then
+    settings.profiles[profile_name] = {}
+    log('Profile `' .. profile_name .. '` created')
+  end
+  -- Add buff to profile
+  if settings.profiles[profile_name][buff_name] == nil then
+    settings.profiles[profile_name][buff_name] = true
+    log('Buff `' .. buff_name .. '` added to profile `' .. profile_name .. '`')
+  else
+    log('Buff `' .. buff_name .. '` already exists in profile `' .. profile_name .. '`')
+  end
+  settings:save('all')
 end
 
-function add_buff(profile_name, buff_name)
+function remove_buff(profile_name, buff_name)
+  -- Remove buff from profile
+  if settings.profiles[profile_name][buff_name] ~= nil then
+    settings.profiles[profile_name][buff_name] = nil
+    log('Buff `' .. buff_name .. '` removed from profile `' .. profile_name .. '`')
+  else
+    log('Buff `' .. buff_name .. '` does not exist in profile `' .. profile_name .. '`')
+  end
+  settings:save('all')
 end

7268f78f808af17c2b7e87c4083a452a1920b26d
Andrew Kassis <kassisaf@gmail.com>
2024-06-24 00:25:46 -0700

    Add constants for shared slot buff ids

---
+++ data/sharedSlots.lua
@@ -0,0 +1,125 @@
+local BAR_ELEMENT = {
+  fire = 100,
+  blizzard = 101,
+  aero = 102,
+  stone = 103,
+  thunder = 104,
+  water = 105,
+  sleep = 106,
+  poison = 107
+}
+
+local BAR_STATUS = {
+  sleep = 106,
+  poison = 107,
+  paralyze = 108,
+  blind = 109,
+  silence = 110,
+  petrify = 111,
+  virus = 112,
+  amnesia = 286
+}
+
+local ENSPELLS = {
+  fire = 94,
+  blizzard = 95,
+  aero = 96,
+  stone = 97,
+  thunder = 98,
+  water = 99,
+  light = 274,
+  fire2 = 277,
+  blizzard2 = 278,
+  aero2 = 279,
+  stone2 = 280,
+  thunder2 = 281,
+  water2 = 282,
+  dark = 288
+  -- sphere = 476,
+  -- drain = 487,
+  -- aspir = 488
+}
+
+local HASTE_FLURRY = {
+  haste = 580,
+  flurry = 581
+}
+
+local RUNES = {
+  ignis = 523,
+  gelus = 524,
+  flabra = 525,
+  tellus = 526,
+  sulpor = 527,
+  unda = 528,
+  lux = 529,
+  tenebrae = 530
+}
+
+local SAMBAS = {
+  drain = 368,
+  aspir = 369,
+  haste = 370
+}
+
+local SPIKES = {
+  blaze = 34,
+  ice = 35,
+  shock = 38,
+  -- damage = 153,
+  dread = 173
+  -- deluge = 573,
+  -- gale = 605,
+  -- clod = 606,
+  -- glint = 607
+}
+
+local STORMS = {
+  fire = 178,
+  hail = 179,
+  wind = 180,
+  sand = 181,
+  thunder = 182,
+  rain = 183,
+  aurora = 184,
+  void = 185,
+  fire2 = 589,
+  hail2 = 590,
+  wind2 = 591,
+  sand2 = 592,
+  thunder2 = 593,
+  rain2 = 594,
+  aurora2 = 595,
+  void2 = 596
+}
+
+local DNC_STANCES = {
+  saber_dance = 410,
+  fan_dance = 411
+}
+
+local NIN_STANCES = {
+  yonin = 420,
+  innin = 421
+}
+
+local RUN_STANCES = {
+  vallation = 531,
+  valiance = 535,
+  liement = 537
+}
+
+local SAM_STANCES = {
+  hasso = 353,
+  seigan = 354
+}
+
+local SCH_STANCES = {
+  light_arts = 358,
+  dark_arts = 359
+}
+
+local WHM_STANCES = {
+  solace = 417,
+  misery = 418
+}
